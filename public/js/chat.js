fc5c791ca9899dbd42fcb4ed37453d6358ee73ba
J-Pard <pardons.jonathan@gmail.com>
2020-06-11 16:12:56 +0200

    404 + chat fix

--- public/js/chat.js
+++ public/js/chat.js
@@ -10,60 +10,68 @@ let socket = io.connect('https://ratpi.herokuapp.com/');
 
 
 let pseudo = prompt("Quel est ton nom, Utilisateur ?");
-socket.emit('newPseudo', pseudo);
 
-BTN.addEventListener("click", (event) => {
-      let message = MSGBOX.value.trim();
-      if(message && message != "" ) {
-            socket.emit('messageToServer', message);
-            MSGBOX.value = "";
-            MSGBOX.placeholder = "Write your message ..."
-      }
-      else {
-            MSGBOX.value = "";
-            MSGBOX.placeholder = "Message invalide !"
-      }
-      event.preventDefault();
-});
+if(pseudo && pseudo.trim() != "" && pseudo.trim() != " " && pseudo != null) {
+      pseudo = pseudo.trim;
+      socket.emit('newPseudo', pseudo);
 
-socket.on('messageToAll', (message) => {
-      if(message.author == socket.pseudo || message.author == pseudo) {
-
-      }
-      else {
-            // Full message
-            let li = document.createElement("li");
-            li.classList.add("crew-member");
-
-            // Author
-            let authorZone = document.createElement("div");
-            authorZone.innerHTML = ">> " + message.author;
-            authorZone.classList.add("author");
-            li.appendChild(authorZone);
-
-            // Text
-            let messageZone = document.createElement("p");
-            messageZone.innerHTML = message.text;
-            messageZone.classList.add("text-message");
-            li.appendChild(messageZone);
-
-            // Style 
-            if(message.author == "You") {
-                  authorZone.innerHTML = "<< " + message.author;
-                  authorZone.classList.add("you");
-                  li.classList.add("text-right");
+      BTN.addEventListener("click", (event) => {
+            let message = MSGBOX.value.trim();
+            if(message && message != "" && message != " " ) {
+                  socket.emit('messageToServer', message);
+                  MSGBOX.value = "";
+                  MSGBOX.placeholder = "Write your message ..."
             }
             else {
-                  li.classList.add("text-left");
+                  MSGBOX.value = "";
+                  MSGBOX.placeholder = "Message invalide !"
             }
-
-            // Including
-            MESSENGER.appendChild(li);
-      }
+            event.preventDefault();
+      });
       
+      socket.on('messageToAll', (message) => {
+            if(message.author == socket.pseudo || message.author == pseudo) {
+      
+            }
+            else {
+                  // Full message
+                  let li = document.createElement("li");
+                  li.classList.add("crew-member");
+      
+                  // Author
+                  let authorZone = document.createElement("div");
+                  authorZone.innerHTML = ">> " + message.author;
+                  authorZone.classList.add("author");
+                  li.appendChild(authorZone);
+      
+                  // Text
+                  let messageZone = document.createElement("p");
+                  messageZone.innerHTML = message.text;
+                  messageZone.classList.add("text-message");
+                  li.appendChild(messageZone);
+      
+                  // Style 
+                  if(message.author == "You") {
+                        authorZone.innerHTML = "<< " + message.author;
+                        authorZone.classList.add("you");
+                        li.classList.add("text-right");
+                  }
+                  else {
+                        li.classList.add("text-left");
+                  }
+      
+                  // Including
+                  MESSENGER.appendChild(li);
+            }
+            
+      
+            // Scroll down
+            let allMsg = document.querySelectorAll("#messenger li");
+            let lastMsg = allMsg[allMsg.length-1];
+            lastMsg.scrollIntoView();
+      });
+}
 
-      // Scroll down
-      let allMsg = document.querySelectorAll("#messenger li");
-      let lastMsg = allMsg[allMsg.length-1];
-      lastMsg.scrollIntoView();
-});
+else {
+      location.href = "/adv";
+}
